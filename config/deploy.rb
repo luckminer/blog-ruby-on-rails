78b3e8f5144c00aacf794095b0ac5ce33a65baaa
luckminer <bloodyblades325@outlook.com>
2019-02-15 11:45:25 +0000

    fix config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,7 @@ set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bundle', 'config', 'public', 'run', 'tmp', 'pids', 'sockets', 'vendor')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
